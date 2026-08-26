85540b47bc1d090d0925ac5c2ca30d88c3ab3e8d
Dana_Ajmi_25 <97060452+dnaj25@users.noreply.github.com>
2026-08-26 21:43:20 +0300

    new

---
+++ txp_print_journal_entries/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Print Journal Entries PDF Report',
+    'version': '1.0',
+    'summary': 'Print PDF voucher and report for Journal Entries',
+    'category': 'Accounting/Accounting',
+    'author': 'dnaj25',
+    'depends': ['account'],
+    'data': [
+        'report/journal_entry_report.xml',
+        'report/journal_entry_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
